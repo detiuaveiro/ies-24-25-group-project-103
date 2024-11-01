338a42b39a5636951240a11cac5a2ce38a55d137
alof04 <afonso.ferreira@ua.pt>
2024-11-01 14:27:50 +0000

    added patient vital service

--- MediSync/src/main/java/org/ies/deti/ua/medisync/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ies.deti.ua.medisync.repository;
-
-import org.ies.deti.ua.medisync.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
