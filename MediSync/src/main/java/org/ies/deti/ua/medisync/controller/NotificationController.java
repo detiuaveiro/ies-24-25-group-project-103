fb6eca9fe85d1abfcc607da37ba84e63a4ae3439
alof04 <afonso.ferreira@ua.pt>
2024-11-02 15:51:24 +0000

    .

---
+++ MediSync/src/main/java/org/ies/deti/ua/medisync/controller/NotificationController.java
@@ -0,0 +1,47 @@
+package org.ies.deti.ua.medisync.controller;
+
+import java.util.List;
+
+import org.ies.deti.ua.medisync.model.Notification;
+import org.ies.deti.ua.medisync.service.NotificationService; // Ensure you have this service created
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @Autowired
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @PostMapping("/send/{userId}")
+    public ResponseEntity<Notification> sendNotification(@PathVariable Long userId, @RequestBody Notification notification) {
+        Notification savedNotification = notificationService.saveNotification(notification);
+        return new ResponseEntity<>(savedNotification, HttpStatus.CREATED);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteNotification(@PathVariable Long id) {
+        notificationService.deleteNotification(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Notification>> getNotificationsByUserId(@PathVariable Long userId) {
+        List<Notification> notifications = notificationService.getNotificationsByUserId(userId);
+        return ResponseEntity.ok(notifications);
+    }
+}
