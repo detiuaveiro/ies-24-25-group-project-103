5f3d1fad0621758abe842812bc0dadcb7a024ad3
alof04 <afonso.ferreira@ua.pt>
2024-11-05 19:43:19 +0000

    corrigir notification

--- MediSync/src/main/java/org/ies/deti/ua/medisync/model/Notification.java
+++ MediSync/src/main/java/org/ies/deti/ua/medisync/model/Notification.java
@@ -39,20 +39,14 @@ public class Notification {
     @JoinColumn(name = "user_id", nullable = false)
     @JsonBackReference
     private User user;
-    
-    public enum Type {
-        DISCHARGE,
-        CLEANING,
-        MEDICATION,
-        DANGERVITALS
-    }
+
 
     @Column(name = "type", nullable = false)
-    private Type type;
+    private String type;
 
     public Notification() {}
 
-    public Notification(String title, String description, Date date, User user, Type type) {
+    public Notification(String title, String description, Date date, User user, String type) {
         this.title = title;
         this.description = description;
         this.date = date;
@@ -100,11 +94,11 @@ public class Notification {
         this.user = user;
     }
 
-    public Type getType() {
+    public String getType() {
         return type;
     }
 
-    public void setType(Type type) {
+    public void setType(String type) {
         this.type = type;
     }
 
