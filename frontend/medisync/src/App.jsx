880bd7c38cb5f4b56e8ea9ba45ad787bb32bde2c
Ricardo <ricardo.alexandre.antunes@ua.pt>
2024-11-15 19:03:34 +0000

    did the modal but the css is a bit wonky

--- frontend/medisync/src/App.jsx
+++ frontend/medisync/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './Login.css';
+import { useState } from 'react';
+
 import Login from './Login';
+import DischargePatient from './DischargePatient';
 
 function App() {
+  const [showModal, setShowModal] = useState(true);
   return (
     <Router>
       <div className="app-container">
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/park" element={<DischargePatient showModal={showModal} setShowModal={setShowModal}/>} />
         </Routes>
       </div>
     </Router>
---
+++ frontend/medisync/src/DischargePatient.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+export default function DischargePatient({showModal, setShowModal}) {    
+    function handleDischarge() {
+        fetch(`/api/v1/patients/${patient.id}/discharge`, {
+        method: 'POST',
+        });
+        setShowModal(false);
+    }
+
+    useEffect(() => {
+        if (showModal) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+    } , [showModal]);
+
+    function handleClose() {
+        setShowModal(false);
+    }
+    
+    return (
+        <>
+            <Modal show={showModal} onHide={handleClose}>
+                <h2>Discharge Patient</h2>
+                <p>Are you sure you want to discharge TEST?</p>
+                <button onClick={handleDischarge}>Discharge</button>
+            </Modal>
+        </>
+    );
+}
